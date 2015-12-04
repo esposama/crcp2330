9100c35fc6cfd4cb440fae44b1b0979ab211e3a5
Amanda Esposito <amandae@smu.edu>
2015-12-03 21:11:24 -0600

    I started over

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,68 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+//(KEY)
+	//@KBD
+	//D = M 
+	//@main
+	//D; JNE 
+	//@KEY 
+	//0;JMP
+
+//(MAIN)
+	//@8191
+	//D = A 
+	//@counter 
+	//M = D 
+
+//(FILL)
+(BEGIN)
+//set i counter to end of screen memory
+@8191
+D=A
+@i
+M=D
+//if (M[24576] ==0 then whiten)
+@24576
+D=M
+@WHITEN
+D;JEQ
+
+//else (M[24576] !=0) then blacken
+@BLACKEN
+0;JMP
+
+(WHITEN)
+	@i 
+	D=M
+	@SCREEN
+	A=A+D
+	M=0
+	//decrement i
+	@i
+	M=M-1
+	//check if i == 0 then jmp to begin
+	@i
+	D=M
+	@BEGIN
+	D;JEQ
+	//else keep whitening
+	@WHITEN
+	0;JMP
+(BLACKEN)
+	@i 
+	D=M
+	@SCREEN
+	A=A+D
+	M=-1
+	//decrement i
+	@i
+	M=M-1
+	//check if i == 0 then jmp to begin
+	@i
+	D=M
+	@BEGIN
+	D;JEQ
+	//else keep whitening
+	@BLACKEN
+	0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2 
+M = 0 
+
+
+(WHILE)
+@R0 //takes value at r0 and puts the value of r0 in d 
+D = M 
+@DONE 
+D;JEQ //if the value of d is equal to zero then you jump to done 
+
+@R1 
+D = M 
+@R2 
+M = M+D //taking the value in r2 and adding it to d 
+@R0
+M = M -1 
+@WHILE
+0;JMP 
+ 
+///this is where you add the code for the addition loop 
+(DONE)
+@DONE 
+0;JMP 
