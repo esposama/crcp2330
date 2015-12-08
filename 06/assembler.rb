c1d60fcf0bf7b86254894b26e338d00c4354d542
Amanda Esposito <amandae@smu.edu>
2015-12-08 01:16:10 -0600

    Finishing the Driver

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -17,6 +17,12 @@ unless is_reabable?(asm_filename)
 	abort("#{asm_filename} not found or is unreadable.")
 end 
 
-puts "The contents of #{asm_filename}"
-asm_file = File.open(asm_filename)
-puts asm_file.read 
+File.open(asm_filename) do |asm_filename| 
+	asm_basename = File.basename(asm_filename, '.asm')
+	path = File.split(asm_filename)[0]
+	hack_filename= "#{path}/#{asm_basename}.hack"
+	File.open(hack_filename) do |hack_file| 
+		assembler = Assembler.new(File.open(asm_filename), hack_file)
+		assembler.assemble! 
+	end 
+end 
